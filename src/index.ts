8e9fc210c56a425a218f2267b4871c6d03acbc3e
Ryan Abis <ryan.abis@gmail.com>
2020-05-26 22:02:31 +0800

    fix server file

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,15 @@ const server = new GraphQLServer({
   resolvers
 })
 
-server.start(() => console.log('Server is running on http://localhost:4000'))
+const options = {
+  port: 8000,
+  endpoint: '/graphql',
+  subscriptions: '/subscriptions',
+  playground: '/playground'
+}
+
+server.start(options, ({ port }) =>
+  console.log(
+    `Server started, listening on port ${port} for incoming requests.`
+  )
+)
