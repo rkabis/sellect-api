6f25e4bbebd0c90120d35d5ae253179c7fea6c17
Ryan Abis <ryan.abis@gmail.com>
2021-01-09 14:51:10 +0800

    use dynamic template for pickup page

--- src/api/links/mutations/createLink.ts
+++ src/api/links/mutations/createLink.ts
@@ -36,12 +36,18 @@ export default async (
   if (createLinkRes.isSuccessful) {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
     const domain = process.env.DASHBOARD_DOMAIN
+    const templateId = process.env.SENDGRID_PICKUP_TEMPLATE_ID
 
     const msg = {
       to: businessEmail,
       from: 'no-reply@sellect.express',
       subject: 'Sellect Express: Successfully Created a Link!',
-      text: `Access your dashboard here: ${domain}/dashboard?id=${createLinkRes.linkId}`
+      templateId: templateId,
+
+      dynamic_template_data: {
+        businessName: businessName,
+        pickupLink: `${domain}/dashboard?id=${createLinkRes.linkId}`
+      }
     }
     sgMail
       .send(msg)
